48cb48d4f0c0dc8e3523e2b89d9687d1563ac563
joshkat <joshua02k@gmail.com>
2023-07-04 23:57:27 -0400

    removed api calls by importing name list

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,17 @@
 import PropType from "prop-types";
+import Pokemon from "/src/pokemon.json";
 import "../css/Card.css";
-import { useEffect, useState } from "react";
 
 export default function Card({ id, handler }) {
-  const [name, setName] = useState("");
-  const [src, setSrc] = useState("");
-
-  useEffect(() => {
-    const endpoint = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((json) => {
-        setSrc(json.sprites.front_default);
-        setName(json.name);
-      })
-      .catch((error) => console.error(error));
-  }, [id]);
-
-  function onClick(e) {
-    e.preventDefault();
+  function onClick() {
     handler(id);
   }
 
   return (
     <>
       <div className="card-root" onClick={onClick}>
-        <h3 className="card-title">{name}</h3>
-        <img className="card-img" src={src} alt={name} />
+        <h3 className="card-title">{Pokemon[id]}</h3>
+        <img className="card-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={Pokemon[id]} /> 
       </div>
     </>
   );
