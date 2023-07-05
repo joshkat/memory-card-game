45e84cc10376cb758951690e2ef0b6d616f855f2
joshkat <joshua02k@gmail.com>
2023-07-04 23:27:27 -0400

    added high score mechanic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import CardsView from "./components/CardsView";
 import "./css/App.css";
@@ -8,10 +8,17 @@ import { noDuplicatesRandArr } from "./modules/noDuplicates";
 function App() {
   const [idArray, setIdArray] = useState(noDuplicatesRandArr(12, 151));
   const [clickedOn, setClickedOn] = useState([]);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    if(clickedOn.length >= highScore){
+      setHighScore(clickedOn.length);
+    }
+  }, [clickedOn, highScore, setHighScore])
 
   return (
     <>
-      <p>Score: {clickedOn.length}</p>
+      <p>Score: {clickedOn.length} <br /> High Score: {highScore}</p>
       <CardsView 
         idArray={idArray} 
         setIdArray={setIdArray} 
